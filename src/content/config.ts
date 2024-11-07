657e510bf96840ab0e071251b0f01afb24691d18
roblesdotdev <httpsnip@gmail.com>
2024-11-07 18:41:16 -0300

    define and configure content collections

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content'
+import { format } from 'date-fns'
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform(str => format(new Date(str), 'MMMM d, yyyy')),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  }),
+})
+
+export const collections = {
+  blog: postsCollection,
+}

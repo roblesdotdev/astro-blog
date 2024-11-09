09a205900b0d83dbef041a9095f6baa570b0c8f3
roblesdotdev <httpsnip@gmail.com>
2024-11-08 23:34:42 -0300

    optimize images

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,15 @@ import { format } from 'date-fns'
 
 const postsCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    author: z.string(),
-    categories: z.array(z.string()),
-    date: z.string().transform(str => format(new Date(str), 'MMMM d, yyyy')),
-    featured: z.boolean().default(false),
-    image: z.string(),
-    title: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      categories: z.array(z.string()),
+      date: z.string().transform(str => format(new Date(str), 'MMMM d, yyyy')),
+      featured: z.boolean().default(false),
+      image: image(),
+      title: z.string(),
+    }),
 })
 
 export const collections = {
